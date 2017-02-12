847bc5e540c9b71e9b635146c933ba94f289bcce
trevorfiez <fieztr@onid.oregonstate.edu>
2017-02-12 14:27:03 -0800

    Finished download script

--- met_download.py
+++ met_download.py
@@ -5,9 +5,7 @@ import urllib2
 import sys
 import getopt
 import csv
-
-
-
+import os
 
 
 #returns list of csv lines that the artist matches
@@ -21,10 +19,37 @@ def match_artist(met_csv, artist):
 	return lines
 	
 
+def download_lines(lines, out_dir):
+	image_names = []
 
+	if not os.path.exists(out_dir):
+		os.path.makedirs(out_dir)
+	
+	for line in lines:
+		res = urllib2.urlopen('http://www.metmuseum.org/art/collection/search/' + str(line[3]))
+		html = res.read()
 
+		offset = html.find("utility-menu__item utility-menu__item--download")
+		offset = html[offset:].find('http') + offset
+		end = html[offset:].find('.jpg') + offset + 4
 
+		image_link = html[offset:end]
 
+		image_name = image_link.split('/')[-1]
+
+		image_path = os.path.join(out_dir, image_name)
+
+		image_file = urllib2.urlopen(image_link)
+
+		with open(image_path, 'wb') as output:
+			output.write(image_file.read())
+
+		image_names.append(image_path)
+
+		print(image_link)
+
+	return image_names
+		
 
 def main(argv):
 	opts, args = getopt.getopt(argv, "i:o:a:", ["csv=", "out=", "artist="])
@@ -41,30 +66,23 @@ def main(argv):
 		elif opt in ("--artist", "-a"):
 			artist = arg
 
-	print((met_csv_file, out_dir, artist))
 	met_csv = csv.reader(open(met_csv_file, 'rb'), delimiter=',')
 
-	'''
-	for row in met_csv:
-		for i in range(len(row)):
-			print((i, row[i]))
-
-		return
-	'''
 	csv_lines = match_artist(met_csv, artist)
 
-	for line in csv_lines:
-		print(csv_lines)
+	painting_names = download_lines(csv_lines, out_dir)
+
+	with open(os.path.join(out_dir, "piece_info.csv"), 'wb') as csv_file:
+		im_writer = csv.writer(csv_file, delimiter=',')
+
+		for row in met_csv:
+			im_writer.writerow(row + ['Image Location'])
+			break
+
+		for i, row in enumerate(csv_lines):
+			im_writer.writerow(row + [painting_names[i]])
 
 	return
-
-
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
