7a30c7cbea3a9581cff21c6ec7e5c09375f33fbc
trevorfiez <fieztr@onid.oregonstate.edu>
2017-03-07 19:19:22 -0800

    Added support for searching through csv by object number

--- met_download.py
+++ met_download.py
@@ -7,14 +7,38 @@ import getopt
 import csv
 import os
 
+from unidecode import unidecode
+
+def check_artists(artist, artists):
+	return artist in artists
+
+def check_types(piece_type, types):
+	if (len(types) == 0):
+		return True
+	else:
+		return piece_type in types
+
+def check_object_num(object_num, num_list):
+	return object_num in num_list
 
 #returns list of csv lines that the artist matches
-def match_artist(met_csv, artist):
+def match_lines(met_csv, artists, types, list_file):
 	lines = []
 
-	for row in met_csv:
-		if (row[14] == artist):
-			lines.append(row)
+	print(artists)
+	if (list_file != ""):
+		f = open(list_file, 'r')
+		obj_num_list = []
+		for line in f:
+			obj_num_list.append(line.strip())
+			
+		for row in met_csv:
+			if (check_object_num(row[0], obj_num_list)):
+				lines.append(row)
+	else:
+		for row in met_csv:
+			if (check_artists(row[14], artists) and check_types(row[24], types)):
+				lines.append(row)
 
 	return lines
 	
@@ -26,20 +50,44 @@ def download_lines(lines, out_dir):
 		os.path.makedirs(out_dir)
 	
 	for line in lines:
-		res = urllib2.urlopen('http://www.metmuseum.org/art/collection/search/' + str(line[3]))
+		res = ""
+		try:
+			res = urllib2.urlopen('http://www.metmuseum.org/art/collection/search/' + line[3].strip())
+		except urllib2.URLError, e:
+			image_names.append(None)
+			print("URL Error")
+			continue
+	
 		html = res.read()
 
 		offset = html.find("utility-menu__item utility-menu__item--download")
+
+		print(offset)
+		if (offset == -1):
+			image_names.append(None)
+			continue
 		offset = html[offset:].find('http') + offset
 		end = html[offset:].find('.jpg') + offset + 4
 
+		if (end - offset > 300):
+			image_names.append(None)
+			print("URL Error")
+			continue
+
 		image_link = html[offset:end]
+		print(image_link)
 
 		image_name = image_link.split('/')[-1]
 
 		image_path = os.path.join(out_dir, image_name)
 
-		image_file = urllib2.urlopen(image_link)
+		image_file = ""
+		try:
+			image_file = urllib2.urlopen(image_link)
+		except urllib2.URLError, e:
+			image_names.append(None)
+			print("URL error")
+			continue		
 
 		with open(image_path, 'wb') as output:
 			output.write(image_file.read())
@@ -52,11 +100,14 @@ def download_lines(lines, out_dir):
 		
 
 def main(argv):
-	opts, args = getopt.getopt(argv, "i:o:a:", ["csv=", "out=", "artist="])
+	opts, args = getopt.getopt(argv, "i:o:a:t:l:", ["csv=", "out=", "artist=", "type=", "list="])
 
 	met_csv_file = ""
 	out_dir = ""
-	artist = ""
+	list_file = ""
+	artists = []
+
+	types = []
 
 	for opt, arg in opts:
 		if opt in ("--csv", "-i"):
@@ -64,11 +115,24 @@ def main(argv):
 		elif opt in ("--out", "-o"):
 			out_dir = arg
 		elif opt in ("--artist", "-a"):
-			artist = arg
+			artists = arg.split(':')
+		elif opt in ("--type", "-t"):
+			types = arg.split(":")
+		elif opt in ("--list", "-l"):
+			list_file = arg
 
 	met_csv = csv.reader(open(met_csv_file, 'rb'), delimiter=',')
 
-	csv_lines = match_artist(met_csv, artist)
+	csv_lines = match_lines(met_csv, artists, types, list_file)
+
+	lines = 0
+	for line in csv_lines:
+		print(line[14] + " " + line[24] + " " + line[3])
+		lines += 1
+
+	print(lines)
+
+	
 
 	painting_names = download_lines(csv_lines, out_dir)
 
@@ -80,6 +144,8 @@ def main(argv):
 			break
 
 		for i, row in enumerate(csv_lines):
+			if (painting_names[i] == None):
+				continue
 			im_writer.writerow(row + [painting_names[i]])
 
 	return
